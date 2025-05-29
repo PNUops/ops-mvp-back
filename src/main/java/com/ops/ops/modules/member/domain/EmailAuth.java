6ad8a01c7729ffe3055343b97caeee8b28521f78
JJimini <min102602@pusan.ac.kr>
2025-05-29 21:58:53 +0900

    feat: 이메일 인증 관련 entity 생성

---
+++ src/main/java/com/ops/ops/modules/member/domain/EmailAuth.java
@@ -0,0 +1,38 @@
+package com.ops.ops.modules.member.domain;
+
+import com.ops.ops.global.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EmailAuth extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Boolean isCorrected;
+
+    @Builder
+    private EmailAuth(final String token, final String email) {
+        this.token = token;
+        this.email = email;
+        this.isCorrected = false;
+    }
+}
