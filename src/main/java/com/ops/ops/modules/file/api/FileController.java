f07e8f4891fc5eed444cebebd011cd403b83312b
student-p <paulmoon00@naver.com>
2025-05-30 18:09:54 +0900

    fix: 팀 썸네일 등록 API 를 팀 도메인으로 이동

--- src/main/java/com/ops/ops/modules/file/api/FileController.java
+++ src/main/java/com/ops/ops/modules/file/api/FileController.java
@@ -1,29 +1,10 @@
 package com.ops.ops.modules.file.api;
 
-import com.ops.ops.modules.file.application.FileService;
-import com.ops.ops.modules.file.application.dto.ThumbnailRequest;
-import com.ops.ops.modules.file.exception.FileException;
-import com.ops.ops.modules.file.exception.FileExceptionType;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class FileController {
 
-    private final FileService fileService;
-
-    @PostMapping("/teams/{teamId}/image/thumbnail")
-    public ResponseEntity saveThumbnailImage(@PathVariable("teamId") Long teamId, ThumbnailRequest thumbnailRequest)
-            throws IOException {
-        if (thumbnailRequest.getImage() == null || thumbnailRequest.getImage().isEmpty()) {
-            throw new FileException(FileExceptionType.NO_IMAGE);
-        }
-        fileService.saveThumbnail(teamId, thumbnailRequest.getImage());
-        return ResponseEntity.noContent().build();
-    }
 }
---
+++ src/main/java/com/ops/ops/modules/team/api/TeamController.java
@@ -0,0 +1,26 @@
+package com.ops.ops.modules.team.api;
+
+import com.ops.ops.modules.file.application.FileCommandService;
+import com.ops.ops.modules.file.application.dto.ThumbnailRequest;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TeamController {
+
+    private final FileCommandService fileCommandService;
+
+    @PostMapping("/teams/{teamId}/image/thumbnail")
+    public ResponseEntity<Void> saveThumbnailImage(@PathVariable("teamId") Long teamId, ThumbnailRequest thumbnailRequest)
+            throws IOException {
+
+        fileCommandService.saveThumbnail(teamId, thumbnailRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
