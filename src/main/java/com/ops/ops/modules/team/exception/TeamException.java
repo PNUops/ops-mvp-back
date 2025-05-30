28ab95789eb2ea1d48fd2f7d3ebe3bddc33b740e
myeowon <135775039+myeowon@users.noreply.github.com>
2025-05-30 13:25:23 +0900

    feat: Team 패키지 내 파일 추가

---
+++ src/main/java/com/ops/ops/modules/team/exception/TeamException.java
@@ -0,0 +1,24 @@
+package com.ops.ops.modules.team.exception;
+
+
+import com.ops.ops.global.base.BaseExceptionType;
+import com.ops.ops.global.base.BaseException;
+
+public class TeamException extends BaseException {
+    private final TeamExceptionType exceptionType;
+
+    public TeamException(final TeamExceptionType exceptionType) {
+        super(exceptionType.errorMessage());
+        this.exceptionType = exceptionType;
+    }
+
+    public TeamException(final TeamExceptionType exceptionType, final String message) {
+        super(message);
+        this.exceptionType = exceptionType;
+    }
+
+    @Override
+    public BaseExceptionType exceptionType() {
+        return exceptionType;
+    }
+}
