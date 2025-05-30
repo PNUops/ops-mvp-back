584af417831a9ecefbdfbfe17428d9005c3a895a
JJimini <min102602@pusan.ac.kr>
2025-05-31 01:47:32 +0900

    feat: 가입 이메일 찾기 api 개발

--- src/main/java/com/ops/ops/modules/member/api/MemberController.java
+++ src/main/java/com/ops/ops/modules/member/api/MemberController.java
@@ -4,16 +4,20 @@ import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 import com.ops.ops.modules.member.application.MemberCommandService;
+import com.ops.ops.modules.member.application.MemberQueryService;
 import com.ops.ops.modules.member.application.dto.request.EmailAuthConfirmRequest;
 import com.ops.ops.modules.member.application.dto.request.EmailAuthRequest;
 import com.ops.ops.modules.member.application.dto.request.PasswordUpdateRequest;
 import com.ops.ops.modules.member.application.dto.request.SignInRequest;
 import com.ops.ops.modules.member.application.dto.request.SignUpRequest;
+import com.ops.ops.modules.member.application.dto.response.EmailFindResponse;
 import com.ops.ops.modules.member.application.dto.response.SignInResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberCommandService memberCommandService;
+    private final MemberQueryService memberQueryService;
 
     @PostMapping("/sign-up")
     public ResponseEntity<Void> signUp(@Valid @RequestBody final SignUpRequest signUpRequest) {
@@ -66,5 +71,11 @@ public class MemberController {
     public ResponseEntity<Void> updatePassword(@Valid @RequestBody final PasswordUpdateRequest passwordUpdateRequest) {
         memberCommandService.updatePassword(passwordUpdateRequest);
         return ResponseEntity.status(NO_CONTENT).build();
+    }
+
+    @GetMapping("/sign-in/{studentId}/email-find")
+    public ResponseEntity<EmailFindResponse> getMyEmail(@PathVariable String studentId) {
+        final EmailFindResponse response = memberQueryService.getMyEmail(studentId);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/ops/ops/modules/member/application/MemberQueryService.java
+++ src/main/java/com/ops/ops/modules/member/application/MemberQueryService.java
@@ -1,5 +1,11 @@
 package com.ops.ops.modules.member.application;
 
+import static com.ops.ops.modules.member.exception.MemberExceptionType.NOT_FOUND_MEMBER;
+
+import com.ops.ops.modules.member.application.dto.response.EmailFindResponse;
+import com.ops.ops.modules.member.domain.Member;
+import com.ops.ops.modules.member.domain.dao.MemberRepository;
+import com.ops.ops.modules.member.exception.MemberException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +14,15 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberQueryService {
+
+    private final MemberRepository memberRepository;
+
+    public EmailFindResponse getMyEmail(final String studentId) {
+        final Member member = getValidateMember(studentId);
+        return new EmailFindResponse(member.getEmail());
+    }
+
+    private Member getValidateMember(final String studentId) {
+        return memberRepository.findByStudentId(studentId).orElseThrow(() -> new MemberException(NOT_FOUND_MEMBER));
+    }
 }
