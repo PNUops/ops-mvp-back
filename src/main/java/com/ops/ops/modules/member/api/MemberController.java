2024b50c5ea78fd9611c99840cd825a93f097361
JJimini <min102602@pusan.ac.kr>
2025-05-30 21:51:02 +0900

    feat : 회원가입 api 개발

--- src/main/java/com/ops/ops/modules/member/api/MemberController.java
+++ src/main/java/com/ops/ops/modules/member/api/MemberController.java
@@ -1,9 +1,23 @@
 package com.ops.ops.modules.member.api;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
+import com.ops.ops.modules.member.application.MemberCommandService;
+import com.ops.ops.modules.member.application.dto.request.SignUpRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberCommandService memberCommandService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<Void> signUp(final SignUpRequest signUpRequest) {
+        memberCommandService.signUp(signUpRequest);
+        return ResponseEntity.status(CREATED).build();
+    }
 }
--- src/main/java/com/ops/ops/modules/member/application/MemberCommandService.java
+++ src/main/java/com/ops/ops/modules/member/application/MemberCommandService.java
@@ -1,6 +1,16 @@
 package com.ops.ops.modules.member.application;
 
+import static com.ops.ops.modules.member.domain.MemberRoleType.ROLE_회원;
+import static com.ops.ops.modules.member.exception.MemberExceptionType.ALREADY_EXIST_EMAIL;
+import static com.ops.ops.modules.member.exception.MemberExceptionType.ALREADY_EXIST_STUDENT_ID;
+
+import com.ops.ops.modules.member.application.dto.request.SignUpRequest;
+import com.ops.ops.modules.member.domain.Member;
+import com.ops.ops.modules.member.domain.dao.MemberRepository;
+import com.ops.ops.modules.member.exception.MemberException;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,4 +18,43 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class MemberCommandService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void signUp(final SignUpRequest request) {
+        final String encodingPassword = passwordEncoder.encode(request.password());
+
+        memberRepository.findByStudentIdAndName(request.studentId(), request.name())
+                .ifPresentOrElse(
+                        member -> member.updateTeamLeaderInfo(request.email(), encodingPassword),
+                        () -> registerNewMember(request.name(), request.studentId(), request.email(), encodingPassword)
+                );
+    }
+
+    private void registerNewMember(final String name, final String studentId, final String email,
+                                   final String password) {
+        checkIsDuplicateEmail(email);
+        checkIsDuplicateStudentId(studentId);
+
+        memberRepository.save(Member.builder()
+                .name(name)
+                .studentId(studentId)
+                .email(email)
+                .password(password)
+                .roles(Set.of(ROLE_회원))
+                .build());
+    }
+
+    private void checkIsDuplicateEmail(final String email) {
+        if (memberRepository.existsByEmail(email)) {
+            throw new MemberException(ALREADY_EXIST_EMAIL);
+        }
+    }
+
+    private void checkIsDuplicateStudentId(final String studentId) {
+        if (memberRepository.existsByStudentId(studentId)) {
+            throw new MemberException(ALREADY_EXIST_STUDENT_ID);
+        }
+    }
 }
