e383e9ac1069e0d8c2a77b682663c8256adff788
student-p <paulmoon00@naver.com>
2025-05-31 22:07:51 +0900

    feat: 팀 프리뷰 이미지 등록 기능구현

--- src/main/java/com/ops/ops/modules/file/exception/FileExceptionType.java
+++ src/main/java/com/ops/ops/modules/file/exception/FileExceptionType.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 
 public enum FileExceptionType implements BaseExceptionType {
     NO_IMAGE(HttpStatus.BAD_REQUEST, "요청에 이미지가 없습니다"),
-
+    EXCEED_PREVIEW_LIMIT(HttpStatus.BAD_REQUEST, "프리뷰 이미지는 6장 이하입니다")
     ;
     private final HttpStatus httpStatus;
     private final String errorMessage;
