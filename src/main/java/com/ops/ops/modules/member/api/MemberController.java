4164d7510ea3a5a491e02e32a0a043d50fb5e385
JJimini <min102602@pusan.ac.kr>
2025-05-31 21:35:48 +0900

    feat: swagger 코드 추가

--- src/main/java/com/ops/ops/modules/member/api/MemberController.java
+++ src/main/java/com/ops/ops/modules/member/api/MemberController.java
@@ -7,6 +7,8 @@ import com.ops.ops.modules.member.application.MemberCommandService;
 import com.ops.ops.modules.member.application.dto.request.EmailAuthConfirmRequest;
 import com.ops.ops.modules.member.application.dto.request.EmailAuthRequest;
 import com.ops.ops.modules.member.application.dto.request.SignUpRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,18 +23,24 @@ public class MemberController {
 
     private final MemberCommandService memberCommandService;
 
+    @Operation(summary = "회원가입", description = "회원가입을 합니다. 팀장은 이미 회원가입 되어 있으므로 이메일과 비밀번호만 업데이트합니다.")
+    @ApiResponse(responseCode = "201", description = "회원가입 성공")
     @PostMapping("/sign-up")
     public ResponseEntity<Void> signUp(@Valid @RequestBody final SignUpRequest signUpRequest) {
         memberCommandService.signUp(signUpRequest);
         return ResponseEntity.status(CREATED).build();
     }
 
+    @Operation(summary = "회원가입 이메일 인증", description = "인증 코드를 발급하고, 메일을 전송합니다.")
+    @ApiResponse(responseCode = "201", description = "이메일 인증 성공")
     @PostMapping("/sign-up/email-auth")
     public ResponseEntity<Void> signUpEmailAuth(@Valid @RequestBody final EmailAuthRequest emailAuthRequest) {
         memberCommandService.signUpEmailAuth(emailAuthRequest);
         return ResponseEntity.status(CREATED).build();
     }
 
+    @Operation(summary = "회원가입 이메일 인증코드 확인", description = "인증코드를 확인합니다.")
+    @ApiResponse(responseCode = "204", description = "확인 성공")
     @PatchMapping("/sign-up/email-auth")
     public ResponseEntity<Void> confirmSignUpEmailAuth(
             @Valid @RequestBody final EmailAuthConfirmRequest emailAuthConfirmRequest) {
