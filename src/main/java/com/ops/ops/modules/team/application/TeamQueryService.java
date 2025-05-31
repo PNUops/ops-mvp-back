b1a6c27d4754d9de12e288ab580a1bc52159c24c
student-p <paulmoon00@naver.com>
2025-05-31 19:45:54 +0900

    feat: 팀 썸네일 이미지 조회 기능 구현

--- src/main/java/com/ops/ops/modules/team/application/TeamQueryService.java
+++ src/main/java/com/ops/ops/modules/team/application/TeamQueryService.java
@@ -1,6 +1,17 @@
 package com.ops.ops.modules.team.application;
 
+import com.ops.ops.modules.file.domain.File;
+import com.ops.ops.modules.file.domain.FileImageType;
+import com.ops.ops.modules.file.domain.dao.FileRepository;
+import com.ops.ops.modules.file.exception.FileException;
+import com.ops.ops.modules.file.exception.FileExceptionType;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,4 +19,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class TeamQueryService {
+
+    private final FileRepository fileRepository;
+
+    public Resource findThumbnail(Long teamId) throws IOException {
+        File thumbnail = fileRepository.findByTeamIdAndType(teamId, FileImageType.THUMBNAIL)
+                .orElseThrow(() -> new FileException(FileExceptionType.NOT_EXISTS_THUMBNAIL));
+        String filePath = thumbnail.getFilePath();
+        Path path = Paths.get(filePath);
+        if (!Files.exists(path)) {
+            throw new FileException(FileExceptionType.NOT_EXISTS_THUMBNAIL);
+        }
+
+        byte[] fileBytes = Files.readAllBytes(path);
+        return new ByteArrayResource(fileBytes);
+    }
 }
