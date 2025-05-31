931c5ae98aa4474f9c4ebb38d56912f66e9bcac3
student-p <paulmoon00@naver.com>
2025-05-31 23:37:28 +0900

    feat: 팀 프리뷰 이미지 조회 기능구현

--- src/main/java/com/ops/ops/modules/file/exception/FileExceptionType.java
+++ src/main/java/com/ops/ops/modules/file/exception/FileExceptionType.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 
 public enum FileExceptionType implements BaseExceptionType {
     NO_IMAGE(HttpStatus.BAD_REQUEST, "요청에 이미지가 없습니다"),
-
+    NOT_EXISTS_PREVIEW(HttpStatus.NOT_FOUND, "존재하지 않는 프리뷰 이미지입니다"),
+    REQUEST_NOT_OWN_IMAGE(HttpStatus.BAD_REQUEST, "자신의 것이 아닌 이미지를 요청하였습니다.")
     ;
     private final HttpStatus httpStatus;
     private final String errorMessage;
--- src/main/java/com/ops/ops/modules/team/api/TeamController.java
+++ src/main/java/com/ops/ops/modules/team/api/TeamController.java
@@ -2,11 +2,15 @@ package com.ops.ops.modules.team.api;
 
 
 import com.ops.ops.modules.team.application.TeamCommandService;
+import com.ops.ops.modules.team.application.TeamQueryService;
 import com.ops.ops.modules.team.application.dto.ThumbnailRequest;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class TeamController {
 
     private final TeamCommandService teamCommandService;
+    private final TeamQueryService teamQueryService;
 
     @PostMapping("/teams/{teamId}/image/thumbnail")
     public ResponseEntity<Void> saveThumbnailImage(@PathVariable Long teamId, ThumbnailRequest thumbnailRequest) throws IOException {
-
         teamCommandService.saveThumbnail(teamId, thumbnailRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @GetMapping("/teams/{teamId}/image/{imageId}")
+    public ResponseEntity<Resource> findPreviewImage(@PathVariable Long teamId, @PathVariable Long imageId) throws IOException {
+        Resource resource = teamQueryService.findPreview(teamId, imageId);
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(resource);
+    }
 }
