ee30b72d4355489a13d75a937d4a42d111442311
pykido <animoto1@naver.com>
2025-06-01 21:58:49 +0900

    refactor : 댓글 조회 API에 MemberName 추가하도록 리팩토링

--- src/main/java/com/ops/ops/modules/team/application/TeamCommentQueryService.java
+++ src/main/java/com/ops/ops/modules/team/application/TeamCommentQueryService.java
@@ -1,5 +1,6 @@
 package com.ops.ops.modules.team.application;
 
+import com.ops.ops.modules.member.domain.dao.MemberRepository;
 import com.ops.ops.modules.team.application.dto.response.TeamCommentResponse;
 import com.ops.ops.modules.team.domain.Team;
 import com.ops.ops.modules.team.domain.TeamComment;
@@ -17,12 +18,23 @@ public class TeamCommentQueryService {
 
 	private final TeamCommentRepository teamCommentRepository;
 	private final TeamCommandService teamCommandService;
+	private final MemberRepository memberRepository;
 
 	public List<TeamCommentResponse> getComments(final Long teamId) {
 		Team team = teamCommandService.validateAndGetTeamById(teamId);
 		List<TeamComment> comments = teamCommentRepository.findAllByTeamId(team.getId());
+
 		return comments.stream()
-			.map(TeamCommentResponse::from)
+			.map(comment -> {
+				String memberName = memberRepository.findNameById(comment.getMemberId());
+				return new TeamCommentResponse(
+					comment.getId(),
+					comment.getDescription(),
+					comment.getMemberId(),
+					memberName,
+					team.getId()
+				);
+			})
 			.toList();
 	}
 }
