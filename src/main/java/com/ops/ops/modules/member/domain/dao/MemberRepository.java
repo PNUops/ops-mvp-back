42409e24da7078a77dc7058391c92e0ab01ef00a
pykido <animoto1@naver.com>
2025-06-02 00:00:52 +0900

    refactor : 포매터 적용

--- src/main/java/com/ops/ops/modules/member/domain/dao/MemberRepository.java
+++ src/main/java/com/ops/ops/modules/member/domain/dao/MemberRepository.java
@@ -1,21 +1,18 @@
 package com.ops.ops.modules.member.domain.dao;
 
 import com.ops.ops.modules.member.domain.Member;
-import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-	Optional<Member> findByEmail(final String email);
+    Optional<Member> findByEmail(final String email);
 
-	Optional<Member> findByStudentIdAndName(final String studentId, final String name);
+    Optional<Member> findByStudentIdAndName(final String studentId, final String name);
 
-	Boolean existsByEmail(final String Email);
+    Boolean existsByEmail(final String Email);
 
-	Boolean existsByStudentId(final String studentId);
-
-	@Query("SELECT m.name FROM Member m WHERE m.id = :id")
-	String findNameById(@Param("id") Long id);
+    Boolean existsByStudentId(final String studentId);
 }
