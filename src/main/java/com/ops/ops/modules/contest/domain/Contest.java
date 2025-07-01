13390904f2e124dd31c616e4b5296a9db8b46eb2
myeowon <myw7931@gmail.com>
2025-07-02 00:19:57 +0900

    feat : 팀 엔티티 작성

--- src/main/java/com/ops/ops/modules/contest/domain/Contest.java
+++ src/main/java/com/ops/ops/modules/contest/domain/Contest.java
@@ -1,4 +1,37 @@
 package com.ops.ops.modules.contest.domain;
 
-public class Contest {
+import com.ops.ops.global.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLRestriction("is_deleted = false")
+@SQLDelete(sql = "UPDATE team SET is_deleted = true where id = ?")
+public class Contest extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String contestName;
+
+    @Column(nullable = false)
+    private Boolean isCurrent;
+
+    @Builder
+    public Contest(String contestName, Boolean isCurrent) {
+        this.contestName = contestName;
+        this.isCurrent = isCurrent;
+    }
 }
