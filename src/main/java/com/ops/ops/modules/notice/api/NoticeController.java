8114f37c0bb153ff6cf8c7d1a1965d1f3663c1d3
JJimini <min102602@pusan.ac.kr>
2025-07-03 19:43:53 +0900

    feat: swagger 관련 코드 추가

--- src/main/java/com/ops/ops/modules/notice/api/NoticeController.java
+++ src/main/java/com/ops/ops/modules/notice/api/NoticeController.java
@@ -5,6 +5,8 @@ import com.ops.ops.modules.notice.application.NoticeQueryService;
 import com.ops.ops.modules.notice.application.dto.request.NoticeRequest;
 import com.ops.ops.modules.notice.application.dto.response.NoticeDetailResponse;
 import com.ops.ops.modules.notice.application.dto.response.NoticeSummaryResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
@@ -32,6 +34,8 @@ public class NoticeController {
     private final NoticeCommandService noticeCommandService;
     private final NoticeQueryService noticeQueryService;
 
+    @Operation(summary = "공지시항 생성", description = "공지사항을 생성합니다. (관리자)")
+    @ApiResponse(responseCode = "201", description = "공지사항 생성 성공")
     @PostMapping
     @Secured("ROLE_관리자")
     public ResponseEntity<Void> createNotice(@Valid @RequestBody final NoticeRequest request) {
@@ -39,6 +43,8 @@ public class NoticeController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "공지시항 수정", description = "공지사항을 수정합니다. (관리자)")
+    @ApiResponse(responseCode = "204", description = "공지사항 수정 성공")
     @PatchMapping("/{noticeId}")
     @Secured("ROLE_관리자")
     public ResponseEntity<Void> updateNotice(@Valid @RequestBody final NoticeRequest request,
@@ -47,6 +53,8 @@ public class NoticeController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "공지시항 삭제", description = "공지사항을 삭제합니다. (관리자)")
+    @ApiResponse(responseCode = "204", description = "공지사항 삭제 성공")
     @DeleteMapping("/{noticeId}")
     @Secured("ROLE_관리자")
     public ResponseEntity<Void> deleteNotice(@PathVariable final Long noticeId) {
@@ -54,11 +62,15 @@ public class NoticeController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "공지시항 상세 목록 조회", description = "공지사항 상세 목록을 조회합니다. (공지사항 페이지)")
+    @ApiResponse(responseCode = "200", description = "공지사항 상세 목록 조회 성공")
     @GetMapping("/{noticeId}")
     public ResponseEntity<NoticeDetailResponse> getNotice(@PathVariable final Long noticeId) {
         return ResponseEntity.ok(noticeQueryService.getNotice(noticeId));
     }
 
+    @Operation(summary = "공지시항 목록 조회", description = "공지사항 목록을 조회합니다. (메인페이지)")
+    @ApiResponse(responseCode = "200", description = "공지사항 목록 조회 성공")
     @GetMapping
     public ResponseEntity<List<NoticeSummaryResponse>> getNotices() {
         return ResponseEntity.ok(noticeQueryService.getNotices());
