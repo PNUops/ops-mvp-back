0a47693b3694b845472fa94e555846a07e92c0c4
myeowon <myw7931@gmail.com>
2025-07-04 21:13:48 +0900

    fix : Team이 contestId 필드를 가짐에 따라 변경

--- src/main/java/com/ops/ops/modules/contest/application/ContestCommandService.java
+++ src/main/java/com/ops/ops/modules/contest/application/ContestCommandService.java
@@ -4,7 +4,7 @@ import com.ops.ops.modules.contest.domain.Contest;
 import com.ops.ops.modules.contest.domain.dao.ContestRepository;
 import com.ops.ops.modules.contest.exception.ContestException;
 import com.ops.ops.modules.contest.exception.ContestExceptionType;
-import java.util.ArrayList;
+import com.ops.ops.modules.team.domain.dao.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,15 +14,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class ContestCommandService {
     private final ContestRepository contestRepository;
+    private final TeamRepository teamRepository;
 
     public void createContest(final String contestName) {
         validateDuplicateContestName(contestName);
         // 만약 제 6회 창의융합 대회를 직접 등록하는 상황일 때 isCurrent를 true로 설정하도록
-        boolean isCurrent = contestName.contains("6회") && contestName.contains("창의융합");
+        final boolean isCurrent = contestName.contains("6회") && contestName.contains("창의융합");
         final Contest contest = Contest.builder()
                 .contestName(contestName)
                 .isCurrent(isCurrent)
-                .teams(new ArrayList<>())
                 .build();
         contestRepository.save(contest);
     }
@@ -46,7 +46,7 @@ public class ContestCommandService {
 
     public void deleteContest(final Long contestId) {
         final Contest contest = validateAndGetContestById(contestId);
-        if (!contest.getTeams().isEmpty()) {
+        if (teamRepository.existsByContestId(contestId)) {
             throw new ContestException(ContestExceptionType.CONTEST_HAS_TEAMS);
         }
         contestRepository.delete(contest);
