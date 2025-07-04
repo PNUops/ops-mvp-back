65702efbf93373da145e6a49bda939c0ba69dee3
JJimini <min102602@pusan.ac.kr>
2025-07-04 22:11:16 +0900

    chore: 사용하지 않는 annotation 삭제 - builder 삭제

--- src/main/java/com/ops/ops/modules/notice/application/dto/response/NoticeDetailResponse.java
+++ src/main/java/com/ops/ops/modules/notice/application/dto/response/NoticeDetailResponse.java
@@ -2,9 +2,7 @@ package com.ops.ops.modules.notice.application.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
-import lombok.Builder;
 
-@Builder
 public record NoticeDetailResponse(
 
         String title,
