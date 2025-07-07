b25690bc15b2d7df7571210e76d0ffb324a996bb
myeowon <myw7931@gmail.com>
2025-07-07 14:48:59 +0900

    fix : 계행 추가 및 오타 수정

--- src/main/java/com/ops/ops/modules/team/application/dto/response/TeamMemberResponse.java
+++ src/main/java/com/ops/ops/modules/team/application/dto/response/TeamMemberResponse.java
@@ -2,5 +2,6 @@ package com.ops.ops.modules.team.application.dto.response;
 
 public record TeamMemberResponse(
         Long teamMemberId,
-        String teamMemberName) {
+        String teamMemberName
+) {
 }
