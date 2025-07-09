fc0c3a6d3672ff9ec8be06c6545c69e808def08a
JJimini <min102602@pusan.ac.kr>
2025-07-09 16:29:52 +0900

    fix: team 관련 코드 리팩토링

--- src/main/java/com/ops/ops/modules/team/application/TeamCommandService.java
+++ src/main/java/com/ops/ops/modules/team/application/TeamCommandService.java
@@ -1,18 +1,22 @@
 package com.ops.ops.modules.team.application;
 
+import static com.ops.ops.modules.contest.exception.ContestExceptionType.ADMIN_ONLY_FOR_PAST_CONTEST;
+import static com.ops.ops.modules.contest.exception.ContestExceptionType.CANNOT_CHANGE_CONTEST_FOR_CURRENT;
+import static com.ops.ops.modules.contest.exception.ContestExceptionType.CANNOT_CREATE_TEAM_OF_CURRENT_CONTEST;
+import static com.ops.ops.modules.contest.exception.ContestExceptionType.CANNOT_UPDATE_TEAM_INFO_FOR_CURRENT;
 import static com.ops.ops.modules.file.domain.FileImageType.PREVIEW;
 import static com.ops.ops.modules.file.exception.FileExceptionType.EXCEED_PREVIEW_LIMIT;
+import static com.ops.ops.modules.member.domain.MemberRoleType.ROLE_관리자;
+import static com.ops.ops.modules.member.domain.MemberRoleType.ROLE_팀장;
 
 import com.ops.ops.global.util.FileStorageUtil;
 import com.ops.ops.modules.contest.application.convenience.ContestConvenience;
 import com.ops.ops.modules.contest.domain.Contest;
 import com.ops.ops.modules.contest.exception.ContestException;
-import com.ops.ops.modules.contest.exception.ContestExceptionType;
 import com.ops.ops.modules.file.domain.FileImageType;
 import com.ops.ops.modules.file.domain.dao.FileRepository;
 import com.ops.ops.modules.file.exception.FileException;
 import com.ops.ops.modules.member.domain.Member;
-import com.ops.ops.modules.member.domain.MemberRoleType;
 import com.ops.ops.modules.team.application.convenience.TeamConvenience;
 import com.ops.ops.modules.team.application.dto.request.TeamCreateRequest;
 import com.ops.ops.modules.team.application.dto.request.TeamDetailUpdateRequest;
@@ -32,14 +36,17 @@ import org.springframework.web.multipart.MultipartFile;
 public class TeamCommandService {
 
     private final FileRepository fileRepository;
-    private final TeamRepository teamRepository;
     private final FileStorageUtil fileStorageUtil;
+
+    private final TeamRepository teamRepository;
+
     private final TeamMemberCommandService teamMemberCommandService;
+
     private final ContestConvenience contestConvenience;
     private final TeamConvenience teamConvenience;
 
     public void saveThumbnailImage(final Long teamId, final MultipartFile image, final FileImageType thumbnailType) {
-        validateExistTeam(teamId);
+        teamConvenience.validateExistTeam(teamId);
         fileRepository.findByTeamIdAndType(teamId, thumbnailType).ifPresent(existingFile -> {
             fileStorageUtil.deleteFile(existingFile.getId());
         });
@@ -47,50 +54,34 @@ public class TeamCommandService {
     }
 
     public void deleteThumbnailImage(Long teamId, FileImageType thumbnailType) {
-        validateAndGetTeamById(teamId);
+        teamConvenience.validateExistTeam(teamId);
         fileRepository.findByTeamIdAndType(teamId, thumbnailType).ifPresent(existingFile -> {
             fileStorageUtil.deleteFile(existingFile.getId());
         });
     }
 
     public void deletePreviewImages(Long teamId, List<Long> ids, FileImageType fileImageType) {
-        validateAndGetTeamById(teamId);
+        //todo : 파일 타입 확인 로직 필요
+        teamConvenience.validateExistTeam(teamId);
         ids.forEach(fileStorageUtil::deleteFile);
     }
 
     public void savePreviewImages(Long teamId, List<MultipartFile> images) {
-        validateExistTeam(teamId);
+        teamConvenience.validateExistTeam(teamId);
         checkPreviewLimit(teamId, images);
         for (MultipartFile image : images) {
             fileStorageUtil.storeFile(image, teamId, PREVIEW);
         }
     }
 
-    private void checkPreviewLimit(Long teamId, List<MultipartFile> images) {
-        long savedCount = fileRepository.countByTeamIdAndType(teamId, PREVIEW);
-        if (savedCount + images.size() > 5) {
-            throw new FileException(EXCEED_PREVIEW_LIMIT);
-        }
-    }
-
-    private void validateExistTeam(final Long teamId) {
-        teamConvenience.getValidateExistTeam(teamId);
-    }
-
-    public Team validateAndGetTeamById(final Long teamId) {
-        return teamConvenience.getValidateExistTeam(teamId);
-    }
-
     public void deleteTeam(final Long teamId) {
-        final Team team = validateAndGetTeamById(teamId);
-        teamRepository.delete(team);
+        teamRepository.delete(teamConvenience.getValidateExistTeam(teamId));
     }
 
     public void updateTeamDetail(final Long teamId, final Member member, final TeamDetailUpdateRequest request) {
-        final Team team = validateAndGetTeamById(teamId);
-
+        final Team team = teamConvenience.getValidateExistTeam(teamId);
         final Contest newContest = contestConvenience.getValidateExistContest(request.contestId());
-        validateTeamContestChange(team, newContest, member, request.teamName(), request.projectName(),
+        checkTeamContestChange(team, newContest, member, request.teamName(), request.projectName(),
                 request.leaderName());
 
         updateLeaderIfChanged(team, request.leaderName());
@@ -102,56 +93,64 @@ public class TeamCommandService {
     public TeamCreateResponse createTeam(TeamCreateRequest request) {
         final Contest contest = contestConvenience.getValidateExistContest(request.contestId());
         if (!contest.isTeamCreatable()) {
-            throw new ContestException(ContestExceptionType.CANNOT_CREATE_TEAM_OF_CURRENT_CONTEST);
+            throw new ContestException(CANNOT_CREATE_TEAM_OF_CURRENT_CONTEST);
         }
 
         final Team team = Team.of(request.leaderName(), request.teamName(), request.projectName(), request.overview(),
                 request.productionPath(), request.githubPath(), request.youTubePath(), request.contestId());
         teamRepository.save(team);
 
-        teamMemberCommandService.assignFakeTeamMember(team, request.leaderName(), Set.of(MemberRoleType.ROLE_팀장));
+        teamMemberCommandService.assignFakeTeamMember(team, request.leaderName(), Set.of(ROLE_팀장));
 
         return TeamCreateResponse.from(team);
     }
 
-    private void validateTeamContestChange(final Team team, final Contest newContest, final Member member,
-                                           final String newTeamName, final String newProjectName,
-                                           final String newLeaderName) {
+    private void checkPreviewLimit(Long teamId, List<MultipartFile> images) {
+        long savedCount = fileRepository.countByTeamIdAndType(teamId, PREVIEW);
+        if (savedCount + images.size() > 5) {
+            throw new FileException(EXCEED_PREVIEW_LIMIT);
+        }
+    }
+
+    private void checkTeamContestChange(final Team team, final Contest newContest, final Member member,
+                                        final String newTeamName, final String newProjectName,
+                                        final String newLeaderName) {
         final Contest oldContest = contestConvenience.getValidateExistContest(team.getContestId());
         if (oldContest.getIsCurrent()) {
-            validateCurrentContest(team, newContest, newTeamName, newProjectName, newLeaderName);
+            checkCurrentContest(team, newContest, newTeamName, newProjectName, newLeaderName);
         } else {
-            validatePastContest(newContest, member);
+            checkPastContest(newContest, member);
         }
     }
 
-    private void validateCurrentContest(final Team team, final Contest newContest, final String newTeamName,
-                                        final String newProjectName, final String newLeaderName) {
+    private void checkCurrentContest(final Team team, final Contest newContest, final String newTeamName,
+                                     final String newProjectName, final String newLeaderName) {
         if (team.isContestChanged(newContest.getId())) {
-            throw new ContestException(ContestExceptionType.CANNOT_CHANGE_CONTEST_FOR_CURRENT);
+            throw new ContestException(CANNOT_CHANGE_CONTEST_FOR_CURRENT);
         }
         if (team.isTeamInfoChanged(newTeamName, newProjectName, newLeaderName)) {
-            throw new ContestException(ContestExceptionType.CANNOT_UPDATE_TEAM_INFO_FOR_CURRENT);
+            throw new ContestException(CANNOT_UPDATE_TEAM_INFO_FOR_CURRENT);
         }
     }
 
-    private void validatePastContest(final Contest newContest, final Member member) {
+    private void checkPastContest(final Contest newContest, final Member member) {
         if (!isAdmin(member)) {
-            throw new ContestException(ContestExceptionType.ADMIN_ONLY_FOR_PAST_CONTEST);
+            throw new ContestException(ADMIN_ONLY_FOR_PAST_CONTEST);
         }
         if (newContest.getIsCurrent()) {
-            throw new ContestException(ContestExceptionType.CANNOT_CREATE_TEAM_OF_CURRENT_CONTEST);
+            throw new ContestException(CANNOT_CREATE_TEAM_OF_CURRENT_CONTEST);
         }
     }
 
     private boolean isAdmin(final Member member) {
-        return member != null && member.getRoles().contains(MemberRoleType.ROLE_관리자);
+        return member != null && member.getRoles().contains(ROLE_관리자);
     }
 
+    // 멤버 삭제와 등록 로직이 복잡하여 예외적으로 command service 의존 허용
     private void updateLeaderIfChanged(Team team, String newLeaderName) {
         if (team.isLeaderNameChanged(newLeaderName)) {
             teamMemberCommandService.removeFakeTeamMemberByName(team, team.getLeaderName());
-            teamMemberCommandService.assignFakeTeamMember(team, newLeaderName, Set.of(MemberRoleType.ROLE_팀장));
+            teamMemberCommandService.assignFakeTeamMember(team, newLeaderName, Set.of(ROLE_팀장));
         }
     }
 }
--- src/main/java/com/ops/ops/modules/team/application/TeamQueryService.java
+++ src/main/java/com/ops/ops/modules/team/application/TeamQueryService.java
@@ -1,7 +1,13 @@
 package com.ops.ops.modules.team.application;
 
 import static com.ops.ops.modules.file.domain.FileImageType.THUMBNAIL;
+import static com.ops.ops.modules.file.exception.FileExceptionType.NOT_EXISTS_PREVIEW;
+import static com.ops.ops.modules.file.exception.FileExceptionType.NOT_EXISTS_THUMBNAIL;
+import static com.ops.ops.modules.file.exception.FileExceptionType.NOT_WEBP_CONVERTED;
+import static com.ops.ops.modules.member.domain.MemberRoleType.ROLE_팀장;
+import static com.ops.ops.modules.member.exception.MemberExceptionType.NOT_FOUND_LEADER;
 import static com.ops.ops.modules.team.exception.TeamExceptionType.NOT_FOUND_TEAM;
+import static com.ops.ops.modules.team.exception.TeamExceptionType.NOT_FOUND_TEAM_MEMBER;
 
 import com.ops.ops.global.util.FileStorageUtil;
 import com.ops.ops.modules.contest.application.convenience.ContestConvenience;
@@ -10,12 +16,10 @@ import com.ops.ops.modules.file.domain.File;
 import com.ops.ops.modules.file.domain.FileImageType;
 import com.ops.ops.modules.file.domain.dao.FileRepository;
 import com.ops.ops.modules.file.exception.FileException;
-import com.ops.ops.modules.file.exception.FileExceptionType;
+import com.ops.ops.modules.member.application.convenience.MemberConvenience;
 import com.ops.ops.modules.member.domain.Member;
-import com.ops.ops.modules.member.domain.MemberRoleType;
-import com.ops.ops.modules.member.domain.dao.MemberRepository;
 import com.ops.ops.modules.member.exception.MemberException;
-import com.ops.ops.modules.member.exception.MemberExceptionType;
+import com.ops.ops.modules.team.application.convenience.TeamConvenience;
 import com.ops.ops.modules.team.application.dto.response.TeamDetailResponse;
 import com.ops.ops.modules.team.application.dto.response.TeamMemberResponse;
 import com.ops.ops.modules.team.application.dto.response.TeamSubmissionStatusResponse;
@@ -27,7 +31,6 @@ import com.ops.ops.modules.team.domain.dao.TeamLikeRepository;
 import com.ops.ops.modules.team.domain.dao.TeamMemberRepository;
 import com.ops.ops.modules.team.domain.dao.TeamRepository;
 import com.ops.ops.modules.team.exception.TeamException;
-import com.ops.ops.modules.team.exception.TeamExceptionType;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -42,17 +45,21 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class TeamQueryService {
-    private final TeamRepository teamRepository;
+
     private final FileRepository fileRepository;
     private final FileStorageUtil fileStorageUtil;
-    private final MemberRepository memberRepository;
+
+    private final TeamRepository teamRepository;
     private final TeamLikeRepository teamLikeRepository;
     private final TeamMemberRepository teamMemberRepository;
+
     private final ContestConvenience contestConvenience;
+    private final MemberConvenience memberConvenience;
+    private final TeamConvenience teamConvenience;
 
     public TeamDetailResponse getTeamDetail(final Long teamId, final Member member) {
         final Team team = teamRepository.findById(teamId)
-                .orElseThrow(() -> new TeamException(TeamExceptionType.NOT_FOUND_TEAM));
+                .orElseThrow(() -> new TeamException(NOT_FOUND_TEAM));
 
         final Contest contest = contestConvenience.getValidateExistContest(team.getContestId());
 
@@ -64,13 +71,51 @@ public class TeamQueryService {
                 .map(File::getId)
                 .toList();
 
-        final boolean isLiked = (member != null)
-                ? teamLikeRepository.findByMemberIdAndTeam(member.getId(), team)
-                .map(TeamLike::getIsLiked)
-                .orElse(false)
-                : false;
+        final boolean isLiked = (member != null) ? teamLikeRepository.findByMemberIdAndTeam(member.getId(), team)
+                .map(TeamLike::getIsLiked).orElse(false) : false;
 
         return TeamDetailResponse.from(contest, team, leaderId, teamMembers, previewIds, isLiked);
+    }
+
+    public Pair<Resource, String> findThumbnailImage(final Long teamId) {
+        teamConvenience.validateExistTeam(teamId);
+        final File findFile = fileRepository.findByTeamIdAndType(teamId, THUMBNAIL)
+                .orElseThrow(() -> new FileException(NOT_EXISTS_THUMBNAIL));
+        checkImageConverted(findFile);
+        return fileStorageUtil.findFileAndType(findFile.getId());
+    }
+
+    public Pair<Resource, String> findPreviewImage(Long teamId, Long imageId) {
+        teamConvenience.validateExistTeam(teamId);
+        final File findFile = fileRepository.findById(imageId).orElseThrow(() -> new FileException(NOT_EXISTS_PREVIEW));
+        checkImageConverted(findFile);
+        return fileStorageUtil.findFileAndType(findFile.getId());
+    }
+
+    public List<TeamSummaryResponse> getAllTeamSummaries(final List<Team> teams, final Member member) {
+        Collections.shuffle(teams);
+
+        final Map<Long, Boolean> likeMap =
+                (member != null) ? teamLikeRepository.findAllByMemberIdAndTeamIn(member.getId(), teams).stream()
+                        .collect(Collectors.toMap(tl -> tl.getTeam().getId(), TeamLike::getIsLiked))
+                        : Collections.emptyMap();
+
+        return teams.stream().map(team -> TeamSummaryResponse.from(team, likeMap.getOrDefault(team.getId(), false)))
+                .toList();
+    }
+
+    public TeamSubmissionStatusResponse getSubmissionStatus(final Member member) {
+        TeamMember teamMember = teamMemberRepository.findByMemberId(member.getId())
+                .orElseThrow(() -> new TeamException(NOT_FOUND_TEAM_MEMBER));
+        Team team = teamMember.getTeam();
+
+        return TeamSubmissionStatusResponse.fromEntity(team);
+    }
+
+    private void checkImageConverted(File findFile) {
+        if (!findFile.isWebpConverted()) {
+            throw new FileException(NOT_WEBP_CONVERTED);
+        }
     }
 
     private Long getLeaderIdByTeamId(final Long teamId) {
@@ -90,65 +135,20 @@ public class TeamQueryService {
         if (memberIds.isEmpty()) {
             return List.of();
         }
-        return memberRepository.findAllById(memberIds).stream()
+        return memberConvenience.findAllById(memberIds)
+                .stream()
                 .filter(member -> !member.isTeamLeader())
                 .map(member -> new TeamMemberResponse(member.getId(), member.getName()))
                 .toList();
     }
 
     private Long getLeaderIdByIds(List<Long> memberIds) {
-        return memberRepository.findAllById(memberIds).stream()
-                .filter(member -> member.getRoles().contains(MemberRoleType.ROLE_팀장))
+        return memberConvenience.findAllById(memberIds)
+                .stream()
+                .filter(member -> member.getRoles().contains(ROLE_팀장))
                 .findFirst()
                 .map(Member::getId)
-                .orElseThrow(() -> new MemberException(MemberExceptionType.NOT_FOUND_LEADER));
-    }
-
-    public Pair<Resource, String> findThumbnailImage(final Long teamId) {
-        validateAndGetTeamById(teamId);
-        final File findFile = fileRepository.findByTeamIdAndType(teamId, THUMBNAIL).orElseThrow(() -> new FileException(
-                FileExceptionType.NOT_EXISTS_THUMBNAIL));
-        checkImageConverted(findFile);
-        return fileStorageUtil.findFileAndType(findFile.getId());
-    }
-
-    public Pair<Resource, String> findPreviewImage(Long teamId, Long imageId) {
-        validateAndGetTeamById(teamId);
-        final File findFile = fileRepository.findById(imageId).orElseThrow(() -> new FileException(
-                FileExceptionType.NOT_EXISTS_PREVIEW));
-        checkImageConverted(findFile);
-        return fileStorageUtil.findFileAndType(findFile.getId());
-    }
-
-    private void checkImageConverted(File findFile) {
-        if (!findFile.isWebpConverted()) {
-            throw new FileException(FileExceptionType.NOT_WEBP_CONVERTED);
-        }
-    }
-
-    public Team validateAndGetTeamById(final Long teamId) {
-        return teamRepository.findById(teamId)
-                .orElseThrow(() -> new TeamException(NOT_FOUND_TEAM));
-    }
-
-    public List<TeamSummaryResponse> getAllTeamSummaries(final List<Team> teams, final Member member) {
-        Collections.shuffle(teams);
-
-        final Map<Long, Boolean> likeMap =
-                (member != null) ? teamLikeRepository.findAllByMemberIdAndTeamIn(member.getId(), teams).stream()
-                        .collect(Collectors.toMap(tl -> tl.getTeam().getId(), TeamLike::getIsLiked))
-                        : Collections.emptyMap();
-
-        return teams.stream().map(team -> TeamSummaryResponse.from(team, likeMap.getOrDefault(team.getId(), false)))
-                .toList();
-    }
-
-    public TeamSubmissionStatusResponse getSubmissionStatus(final Member member) {
-        TeamMember teamMember = teamMemberRepository.findByMemberId(member.getId())
-                .orElseThrow(() -> new TeamException(TeamExceptionType.NOT_FOUND_TEAM_MEMBER));
-        Team team = teamMember.getTeam();
-
-        return TeamSubmissionStatusResponse.fromEntity(team);
+                .orElseThrow(() -> new MemberException(NOT_FOUND_LEADER));
     }
 
 }
