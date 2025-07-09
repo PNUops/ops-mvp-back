da0a28a8581556daf74ce2bcbf782b6c3e12c105
JJimini <min102602@pusan.ac.kr>
2025-07-09 21:10:13 +0900

    chore: file Repository 개행 추가

--- src/main/java/com/ops/ops/modules/file/domain/dao/FileRepository.java
+++ src/main/java/com/ops/ops/modules/file/domain/dao/FileRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileRepository extends JpaRepository<File, Long> {
     List<File> findAllByTeamIdAndType(Long teamId, FileImageType type);
+
     Optional<File> findByTeamIdAndType(Long teamId, FileImageType type);
+    
     long countByTeamIdAndType(Long teamId, FileImageType type);
 }
