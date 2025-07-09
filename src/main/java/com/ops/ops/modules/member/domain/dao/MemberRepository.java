11956686bcead06696fd2b72dc845e973bb85175
JJimini <min102602@pusan.ac.kr>
2025-07-09 21:11:44 +0900

    chore: member 관련 repository 개행 삭제

--- src/main/java/com/ops/ops/modules/member/domain/dao/MemberRepository.java
+++ src/main/java/com/ops/ops/modules/member/domain/dao/MemberRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
     Optional<Member> findByEmail(final String email);
 
     Optional<Member> findByStudentId(final String studentId);
