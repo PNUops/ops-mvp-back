8f927fd14b50f8fb1fcc1d5d6426b31137533c94
JJimini <min102602@pusan.ac.kr>
2025-07-17 18:46:20 +0900

    fix: 공지사항 응답 형태 수정

--- src/main/java/com/ops/ops/modules/notice/application/dto/response/NoticeDetailResponse.java
+++ src/main/java/com/ops/ops/modules/notice/application/dto/response/NoticeDetailResponse.java
@@ -1,13 +1,11 @@
 package com.ops.ops.modules.notice.application.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 
 public record NoticeDetailResponse(
 
         String title,
         String description,
-        @JsonFormat(pattern = "yyyy년 MM월 dd일 EEEE HH:mm", locale  = "ko")
         LocalDateTime updatedAt
 ) {
 }
--- src/main/java/com/ops/ops/modules/notice/application/dto/response/NoticeSummaryResponse.java
+++ src/main/java/com/ops/ops/modules/notice/application/dto/response/NoticeSummaryResponse.java
@@ -1,13 +1,11 @@
 package com.ops.ops.modules.notice.application.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 
 public record NoticeSummaryResponse(
 
         Long noticeId,
         String title,
-        @JsonFormat(pattern = "yyyy년 MM월 dd일 EEEE HH:mm", locale  = "ko")
         LocalDateTime updatedAt
 ) {
 }
