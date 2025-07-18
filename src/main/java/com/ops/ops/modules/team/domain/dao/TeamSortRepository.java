464b1ce86420ec0d60d490b4caa9e5f1784aeb85
JJimini <min102602@pusan.ac.kr>
2025-07-18 14:15:15 +0900

    feat: 팀 정렬 관련 Repository 생성

---
+++ src/main/java/com/ops/ops/modules/team/domain/dao/TeamSortRepository.java
@@ -0,0 +1,7 @@
+package com.ops.ops.modules.team.domain.dao;
+
+import com.ops.ops.modules.team.domain.TeamSort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamSortRepository extends JpaRepository<TeamSort, Long> {
+}
