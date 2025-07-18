2f549671dcec88d4b0c9afda2baccff7410a47c1
JJimini <min102602@pusan.ac.kr>
2025-07-18 14:23:58 +0900

    refactor: teamSort 관련 중복 코드가 발생하여 TeamSortConvenience 생성 후 코드 리팩토링

--- src/main/java/com/ops/ops/modules/team/application/TeamCommandService.java
+++ src/main/java/com/ops/ops/modules/team/application/TeamCommandService.java
@@ -8,7 +8,6 @@ import static com.ops.ops.modules.file.domain.FileImageType.PREVIEW;
 import static com.ops.ops.modules.file.exception.FileExceptionType.EXCEED_PREVIEW_LIMIT;
 import static com.ops.ops.modules.member.domain.MemberRoleType.ROLE_관리자;
 import static com.ops.ops.modules.member.domain.MemberRoleType.ROLE_팀장;
-import static com.ops.ops.modules.team.exception.TeamExceptionType.EXIST_ONLY_ONE_ABOUT_TEAM_SORT;
 
 import com.ops.ops.global.util.FileStorageUtil;
 import com.ops.ops.modules.contest.application.convenience.ContestConvenience;
@@ -21,6 +20,7 @@ import com.ops.ops.modules.member.application.convenience.MemberConvenience;
 import com.ops.ops.modules.member.domain.Member;
 import com.ops.ops.modules.team.application.convenience.TeamConvenience;
 import com.ops.ops.modules.team.application.convenience.TeamMemberConvenience;
+import com.ops.ops.modules.team.application.convenience.TeamSortConvenience;
 import com.ops.ops.modules.team.application.dto.request.TeamCreateRequest;
 import com.ops.ops.modules.team.application.dto.request.TeamDetailUpdateRequest;
 import com.ops.ops.modules.team.application.dto.request.TeamSortRequest;
@@ -28,8 +28,6 @@ import com.ops.ops.modules.team.application.dto.response.TeamCreateResponse;
 import com.ops.ops.modules.team.domain.Team;
 import com.ops.ops.modules.team.domain.TeamSort;
 import com.ops.ops.modules.team.domain.dao.TeamRepository;
-import com.ops.ops.modules.team.domain.dao.TeamSortRepository;
-import com.ops.ops.modules.team.exception.TeamException;
 import java.util.List;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
@@ -46,7 +44,6 @@ public class TeamCommandService {
     private final FileStorageUtil fileStorageUtil;
 
     private final TeamRepository teamRepository;
-    private final TeamSortRepository teamSortRepository;
 
     private final TeamMemberCommandService teamMemberCommandService;
 
@@ -54,6 +51,7 @@ public class TeamCommandService {
     private final TeamConvenience teamConvenience;
     private final TeamMemberConvenience teamMemberConvenience;
     private final MemberConvenience memberConvenience;
+    private final TeamSortConvenience teamSortConvenience;
 
     public void saveThumbnailImage(final Long teamId, final MultipartFile image, final FileImageType thumbnailType) {
         teamConvenience.validateExistTeam(teamId);
@@ -126,10 +124,8 @@ public class TeamCommandService {
     }
 
     public void updateTeamSort(final TeamSortRequest request) {
-        checkExistTeamSort();
-
-        final TeamSort teamSort = teamSortRepository.findById(1L)
-                .orElseThrow(() -> new TeamException(EXIST_ONLY_ONE_ABOUT_TEAM_SORT));
+        teamSortConvenience.validateExistTeamSort();
+        final TeamSort teamSort = teamSortConvenience.getValidateExistTeamSort();
 
         teamSort.updateSortType(request.mode());
     }
@@ -186,12 +182,6 @@ public class TeamCommandService {
     private void checkIsTeamCreatable(final Contest contest) {
         if (!contest.isTeamCreatable()) {
             throw new ContestException(CANNOT_CREATE_TEAM_OF_CURRENT_CONTEST);
-        }
-    }
-
-    private void checkExistTeamSort() {
-        if (!teamSortRepository.existsById(1L)) {
-            teamSortRepository.save(TeamSort.builder().build());
         }
     }
 }
