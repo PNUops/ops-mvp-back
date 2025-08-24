de3a715dd48d92a8e9733df41edb79c8c250c9dd
student-p <paulmoon00@naver.com>
2025-08-24 12:49:37 +0900

    fix: 검증 어노테이션 수정

--- src/main/java/com/ops/ops/modules/contest/application/dto/request/VoteUpdateRequest.java
+++ src/main/java/com/ops/ops/modules/contest/application/dto/request/VoteUpdateRequest.java
@@ -1,12 +1,12 @@
 package com.ops.ops.modules.contest.application.dto.request;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 public record VoteUpdateRequest(
-        @NotEmpty(message = "투표 시작 시각을 정해야 합니다")
+        @NotNull(message = "투표 시작 시각을 정해야 합니다")
         LocalDateTime voteStartAt,
-        @NotEmpty(message = "투표 종료 시각을 정해야 합니다")
+        @NotNull(message = "투표 종료 시각을 정해야 합니다")
         LocalDateTime voteEndAt
 ) {
 }
