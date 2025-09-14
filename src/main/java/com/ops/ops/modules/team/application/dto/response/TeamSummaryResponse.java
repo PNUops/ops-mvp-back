9846aa61a1f0cd9831147a73699f18585f366de0
pykido <animoto1@naver.com>
2025-09-15 01:47:06 +0900

    refactor : 수상 색도 추가

--- src/main/java/com/ops/ops/modules/team/application/dto/response/TeamSummaryResponse.java
+++ src/main/java/com/ops/ops/modules/team/application/dto/response/TeamSummaryResponse.java
@@ -7,13 +7,15 @@ public record TeamSummaryResponse(
         String teamName,
         String projectName,
         boolean isLiked,
-        String awardName) {
+        String awardName,
+        String awardColor) {
     public static TeamSummaryResponse from(Team team, boolean isLiked) {
         return new TeamSummaryResponse(
                 team.getId(),
                 team.getTeamName(),
                 team.getProjectName(),
                 isLiked,
-                team.getAwardName());
+                team.getAwardName(),
+                team.getAwardColor());
     }
 }
