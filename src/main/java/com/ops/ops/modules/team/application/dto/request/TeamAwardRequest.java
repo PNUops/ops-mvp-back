43b5608b535879d1cd41502be3f57d3d3af1d406
pykido <animoto1@naver.com>
2025-09-15 23:31:52 +0900

    refactor : 포매터 수동 적용

--- src/main/java/com/ops/ops/modules/team/application/dto/request/TeamAwardRequest.java
+++ src/main/java/com/ops/ops/modules/team/application/dto/request/TeamAwardRequest.java
@@ -1,3 +1,7 @@
 package com.ops.ops.modules.team.application.dto.request;
 
-public record TeamAwardRequest(String awardName, String awardColor) {}
+public record TeamAwardRequest(
+        String awardName,
+        String awardColor
+) {
+}
